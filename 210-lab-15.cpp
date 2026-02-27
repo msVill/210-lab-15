a009d0c9415e70d3b6953640273fac0559df611a
msVill <msv.ltmc@gmail.com>
2026-02-26 17:09:21 -0800

    Added: comments to remember what I need to do, and the starting code for class Movie and main()

---
+++ 210-lab-15.cpp
@@ -0,0 +1,17 @@
+// comsc 210 | lab 15 | Martha Stephanie Villalta
+// class Movie: screenWriter, yearReleased, title as private class members
+// getters and setters public class members for screenWriter, yearReleased, and title.
+// print method (could not determine from the requirements whether as a class member or as a global function. 
+// Needs to read data IN from an input file in specified order: title, yearReleased, screenWriter.
+// read/pass this data into temporary Movie object.
+// append that object to a container: <array> / <vector>
+// Towards end of main(): output data in array/vector.
+
+class Movie {
+
+};
+
+int main() {
+
+    return 0;
+}
